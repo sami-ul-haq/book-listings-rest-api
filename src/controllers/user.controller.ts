84c867ef20b3bc19a16052f1b27c9c1e2ef21c56
Sami ul Haq <samiulhaq1899@gmail.com>
2024-06-02 17:20:48 +0500

    created async handler for async operations

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,42 +4,44 @@ import { userModel } from "../models/user.model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { config } from "../config/config";
+import { asyncHandler } from "../../utils/asyncHandler";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  const { name, email, password } = req.body;
+const createUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
 
-  // Validation
-  if (!name || !email || !password) {
-    const error = createHttpError(400, "All fields are required");
-    return next(error);
+    // Validation
+    if (!name || !email || !password) {
+      return next(createHttpError(400, "All fields are required"));
+    }
+
+    //   Database Call
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+      return next(createHttpError(400, "User already exists with this email"));
+    }
+
+    //   Password Hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //   New user creatiion
+    const newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    //   Token generation
+    const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({
+      message: "User Registered Successfully",
+      accessToken: token,
+    });
   }
-
-  //   Database Call
-  const user = await userModel.findOne({ email });
-
-  if (user) {
-    const error = createHttpError(400, "User already exists with this email");
-    return next(error);
-  }
-
-  //   Password Hashing
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  //   New user creatiion
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
-
-  //   Token generation
-  const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-  });
-
-  res.status(200).json({
-    accessToken: token,
-  });
-};
+);
 
 export { createUser };
