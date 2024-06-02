bf764bf1246f1576db72d333b73e49983414c763
Sami ul Haq <samiulhaq1899@gmail.com>
2024-06-02 17:02:52 +0500

    Added jwt token funtionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,16 +3,18 @@ import createHttpError from "http-errors";
 import { userModel } from "../models/user.model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
-  // validation
+
+  // Validation
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
-  //   database call
 
+  //   Database Call
   const user = await userModel.findOne({ email });
 
   if (user) {
@@ -20,16 +22,23 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
 
+  //   Password Hashing
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  //   New user creatiion
   const newUser = await userModel.create({
     name,
     email,
     password: hashedPassword,
   });
 
+  //   Token generation
+  const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
   res.status(200).json({
-    id: newUser._id,
+    accessToken: token,
   });
 };
 
