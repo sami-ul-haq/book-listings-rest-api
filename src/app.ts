52840f5582039a74d54de127aaa9d464b9efb896
Sami ul Haq <samiulhaq1899@gmail.com>
2024-06-02 15:45:25 +0500

    Add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
-
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({ mxg: "Wecome to the Book Listings" });
+});
 
+// global Error Handler
+app.use(globalErrorHandler);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,8 +3,9 @@ import { config as conf } from "dotenv";
 conf();
 
 const _config = {
-    port: process.env.PORT,
-    databaseURL: process.env.MONGO_CONNECTION_STRING
-}
+  port: process.env.PORT,
+  databaseURL: process.env.MONGO_CONNECTION_STRING,
+  env: process.env.NODE_ENV,
+};
 
 export const config = Object.freeze(_config);
